4750928f8db7e832b2161304aaf82491bcf1124f
hodak <kaczor555@gmail.com>
2016-05-14 14:14:16 +0200

    Refactor: add private serialize_project function in view

--- web/views/api/projects_view.ex
+++ web/views/api/projects_view.ex
@@ -2,19 +2,20 @@ defmodule TodoElixir.Api.ProjectsView do
   use TodoElixir.Web, :view
 
   def render("index.json", %{projects: projects}) do
-    serialized_projects = for project <- projects do
-      Map.take(project, [:id, :name])
-    end
-
+    serialized_projects = Enum.map(projects, &serialize_project/1)
     %{projects: serialized_projects}
   end
 
   def render("show.json", %{project: project}) do
-    %{project: Map.take(project, [:id, :name]) }
+    %{project: serialize_project(project)}
   end
 
   def render("error.json", %{changeset: changeset}) do
     errors = Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
     %{errors: errors}
+  end
+
+  defp serialize_project(project) do
+    Map.take(project, [:id, :name])
   end
 end
