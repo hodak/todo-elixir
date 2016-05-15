bd1a92501ae85e438e645c6cc43653e1a97157dc
hodak <kaczor555@gmail.com>
2016-05-15 13:53:21 +0200

    Refactor: add get_project function, use pattern matching inst. of ifs

--- web/controllers/api/task_controller.ex
+++ web/controllers/api/task_controller.ex
@@ -3,35 +3,32 @@ defmodule TodoElixir.Api.TaskController do
   alias TodoElixir.{Project, Task}
 
   def index(conn, %{"project_id" => project_id}) do
-    project = Repo.one from p in Project,
-      where: p.id == ^project_id
-
-    if project do
-      tasks = Repo.all from t in Task,
-        where: t.project_id == ^project.id,
-        where: t.completed == false
-
-      render(conn, "index.json", tasks: tasks)
-    else
-      send_resp(conn, 404, "")
+    case get_project(project_id) do
+      nil -> send_resp(conn, 404, "")
+      project ->
+        tasks = Repo.all from t in Task,
+          where: t.project_id == ^project.id,
+          where: t.completed == false
+        render(conn, "index.json", tasks: tasks)
     end
   end
 
   def create(conn, %{"project_id" => project_id, "task" => task_params}) do
-    project = Repo.one from p in Project,
-      where: p.id == ^project_id
+    case get_project(project_id) do
+      nil -> send_resp(conn, 404, "")
+      project ->
+        changeset = Task.changeset(%Task{project_id: project.id}, task_params)
 
-    if project do
-      changeset = Task.changeset(%Task{project_id: project.id}, task_params)
-
-      if changeset.valid? do
-        task = Repo.insert!(changeset)
-        render(conn, "show.json", task: task)
-      else
-        # pending
-      end
-    else
-      send_resp(conn, 404, "")
+        if changeset.valid? do
+          task = Repo.insert!(changeset)
+          render(conn, "show.json", task: task)
+        else
+          # pending
+        end
     end
+  end
+
+  defp get_project(project_id) do
+    Repo.one from p in Project, where: p.id == ^project_id
   end
 end
